94a1da25676efc2cee77551356ef38b08831956f
kmh <minghaokong77@163.com>
2021-12-23 00:45:58 +0800

    完成lab2

--- lab2/scheduler.cc
+++ lab2/scheduler.cc
@@ -57,9 +57,9 @@ Scheduler::ReadyToRun (Thread *thread)
 
     thread->setStatus(READY);
 //    readyList->Append((void *)thread);
-    //通过优先级排序并插入
+    //通过优先级排序并插入,等待调入
     readyList->SortedInsert(thread, thread->get_priority());
-    //进行相应的调度
+    //如果是抢占的则需要重新竞争,否则直接插队排入即可
 //    if (thread != currentThread && thread->get_priority() > currentThread->get_priority()) {
 //        currentThread->Yield();
 //    }
